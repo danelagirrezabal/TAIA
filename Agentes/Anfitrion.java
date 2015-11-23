7b39cd828d9bd81d62e7c522c9b9417d7c578e0f
Maitane Azkue <maitane07@gmail.com>
2015-11-23 19:05:21 +0100

    Lehenego bertsiyua

--- Agentes/Anfitrion.java
+++ Agentes/Anfitrion.java
@@ -1,10 +1,17 @@
 package Agentes;
 
+import java.util.concurrent.CyclicBarrier;
+
 import jade.core.Agent;
+import jade.core.behaviours.CyclicBehaviour;
+import jade.core.behaviours.SequentialBehaviour;
+import jade.core.behaviours.TickerBehaviour;
 import jade.domain.DFService;
 import jade.domain.FIPAException;
 import jade.domain.FIPAAgentManagement.DFAgentDescription;
 import jade.domain.FIPAAgentManagement.ServiceDescription;
+import jade.lang.acl.ACLMessage;
+import jade.lang.acl.MessageTemplate;
 
 public class Anfitrion extends Agent {
 
@@ -19,14 +26,33 @@ public class Anfitrion extends Agent {
 		try {
 			DFService.register(this, description);
 			System.out.println(getAID().getLocalName()
-					+ " ha llegado. ¡¡¡Empieza la fiesta!!!");
+					+ " esta preparado para recibir a todos los invitados.");
 			System.out.println();
 		} catch (FIPAException fe) {
 			fe.printStackTrace();
 		}
+		SequentialBehaviour seqBehaviour = new SequentialBehaviour();
+		seqBehaviour.addSubBehaviour(new TickerBehaviour(this, 10000) {
+			/**
+			 * 
+			 */
+			private static final long serialVersionUID = 1L;
+
+			protected void onTick() {
+				ACLMessage msg = blockingReceive(MessageTemplate
+						.MatchPerformative(ACLMessage.INFORM));
+				if (msg.getContent().equalsIgnoreCase("hola")) {
+					System.out.println("(" + getLocalName() + ") Buenos dias "+ msg.getSender().getLocalName());
+				}
+			}
+		});
+		addBehaviour(seqBehaviour);
 	}
-	
-	
-	
+
+	private class escuchar extends CyclicBehaviour {
+		public void action() {
+
+		}
+	}
 
 }
---
+++ Agentes/Invitado1.java
@@ -0,0 +1,212 @@
+package Agentes;
+
+import java.util.Random;
+
+import jade.core.AID;
+import jade.core.Agent;
+import jade.core.behaviours.Behaviour;
+import jade.core.behaviours.CyclicBehaviour;
+import jade.core.behaviours.SequentialBehaviour;
+import jade.core.behaviours.WakerBehaviour;
+import jade.domain.DFService;
+import jade.domain.FIPAException;
+import jade.domain.FIPAAgentManagement.DFAgentDescription;
+import jade.domain.FIPAAgentManagement.ServiceDescription;
+import jade.lang.acl.ACLMessage;
+import jade.lang.acl.MessageTemplate;
+
+public class Invitado1 extends Agent{
+	
+	private AID[] listaInv;
+	
+	
+	
+	protected void setup(){
+		doWait(10000);
+		// Registrar party-host
+		DFAgentDescription description = new DFAgentDescription();
+		description.setName(getAID());
+		ServiceDescription serviceDescription = new ServiceDescription();
+		serviceDescription.setType("party-guest");
+		serviceDescription.setName(getAID().getLocalName());
+		description.addServices(serviceDescription);
+		try {
+			DFService.register(this, description);
+			 System.out.println(getAID().getLocalName() + " ha llegado.");
+			 System.out.println();
+		} catch (FIPAException fe) {
+			fe.printStackTrace();
+		}
+		invitados();
+		//Saludar a todos
+		addBehaviour(new Saludar());
+		addBehaviour(new Responder());
+		
+		
+		
+		
+	}
+	
+	
+	private class Saludar extends Behaviour {
+
+		MessageTemplate tratarRespuestasSaludo;
+		boolean fin = false;
+
+		public void onStart() {
+			System.out
+					.println("---------------------------------------------------------------------------------");
+			System.out
+					.println("["
+							+ getLocalName()
+							+ "] : He entrado... Vamos a saludar a los invitados que están en la sala.");
+			System.out
+					.println("---------------------------------------------------------------------------------\n");
+		}
+
+		public void action() {
+
+			for (int i = 0; i < listaInv.length; i++) {
+				ACLMessage cfp = new ACLMessage(ACLMessage.CFP);
+				cfp.addReceiver(listaInv[i]);
+				cfp.setConversationId("Saludo");
+				cfp.setContent("Hola");
+				System.out.println("[" + getLocalName()
+						+ "]: Que tal va la noche "
+						+ listaInv[i].getLocalName() + "!!!");
+				send(cfp);
+				doWait(2000);
+				tratarRespuestasSaludo = MessageTemplate
+						.MatchConversationId("ResponderSaludo");
+				ACLMessage respuesta = myAgent.receive(tratarRespuestasSaludo);
+				if (respuesta != null) {
+					// if (respuesta.getPerformative() == ACLMessage.PROPOSE) {
+					System.out.println("[" + getLocalName()
+							+ "]: Ya hablaremos después "
+							+ respuesta.getSender().getLocalName() + "!!");
+					System.out.println();
+					// }
+				} else {
+					block();
+				}
+			}
+			if (listaInv.length == 0) {
+				System.out
+						.println("["
+								+ getLocalName()
+								+ "] : Soy el primero en llegar. ¡¡Mejor voy a saludar al anfitrión!!\n");
+			}
+			doWait(2000);
+
+			//Saluda anfitrion
+			ACLMessage msg = new ACLMessage(ACLMessage.INFORM);
+			msg.addReceiver(new AID("Anfitrion", AID.ISLOCALNAME));
+			msg.setContent("hola");
+			System.out.println("("+getAID().getLocalName()+") Hola anfitrion, buenos dias.\n");
+			send(msg);
+			fin = true;
+
+		}
+
+		public boolean done() {
+			return fin;
+		}
+
+
+	}
+	
+	public class Responder extends CyclicBehaviour{
+		MessageTemplate tratarSaludos;
+		MessageTemplate tratarComida;
+
+		public void action() {
+			tratarSaludos = MessageTemplate.MatchConversationId("Saludo");
+//			tratarComida = MessageTemplate.MatchConversationId("Comida");
+			ACLMessage msg = myAgent.receive(tratarSaludos);
+//			ACLMessage msg2 = myAgent.receive(tratarComida);
+			if (msg != null) {
+				// Se ha recibido un mensaje de Saludo y lo procesamos
+				ACLMessage reply = msg.createReply();
+				reply.setConversationId("ResponderSaludo");
+				reply.setContent("Que pasa troncoo");
+				// reply.setPerformative(ACLMessage.CFP);
+				System.out.println("[" + getLocalName() + "]: ¡¡¡Que pasa "
+						+ msg.getSender().getLocalName() + "!!!");
+				send(reply);
+			} 
+//			else if (msg2 != null) {
+//				// Se ha recibido un mensaje de Comida y lo procesamos
+//				ACLMessage reply2 = msg2.createReply();
+//				reply2.setConversationId("ResponderComida");
+//				Random rand = new Random();
+//				int ranNum = rand.nextInt(10 - 1 + 1) + 1;
+//				if (ranNum == 3) {
+//					reply2.setContent("Gin Tonic");
+//					bebida++;
+//					tragos++;
+//				} else if (ranNum <= 2) {
+//					reply2.setContent("Cerbeza");
+//					bebida++;
+//					tragos++;
+//				} else if (ranNum == 4) {
+//					reply2.setContent("Patxaran");
+//					bebida++;
+//					tragos++;
+//				} else if (ranNum == 5) {
+//					reply2.setContent("Martini");
+//					bebida++;
+//					tragos++;
+//				} else if (ranNum == 5) {
+//					reply2.setContent("Ron");
+//					bebida++;
+//					tragos++;
+//				} else if (ranNum == 6) {
+//					reply2.setContent("Whisky");
+//					bebida++;
+//					tragos++;
+//				} else if (ranNum == 7) {
+//					reply2.setContent("Agua");
+//					bebida--;
+//				} else if (ranNum > 7) {
+//					reply2.setContent("Comida");
+//					comida++;
+//				}
+//				reply2.setPerformative(ACLMessage.REQUEST);
+//				System.out.println("[" + getLocalName() + "]: ¡¡¡Dame un poco de "
+//						+ reply2.getContent() + " "
+//						+ msg2.getSender().getLocalName() + "!!!");
+//				send(reply2);
+//			} 
+			else {
+				block();
+			}
+		}
+	}
+	
+	private void invitados(){
+		DFAgentDescription agentDescription = new DFAgentDescription();
+		ServiceDescription serviceDescription = new ServiceDescription();
+		serviceDescription.setType("party-guest");
+		agentDescription.addServices(serviceDescription);
+
+		try {
+			DFAgentDescription[] result = DFService.search(this,
+					agentDescription);
+			listaInv = new AID[result.length - 1];
+			// System.out.println("[" + getLocalName() + "] Invitados sala:");
+			int f = 0;
+			for (int i = 0; i < result.length; ++i) {
+				if (result[i].getName().getLocalName()
+						.equals(this.getLocalName()) != true) {
+					listaInv[f] = result[i].getName();
+					// System.out.println(result[i].getName().getLocalName());
+					f++;
+				}
+			}
+		} catch (FIPAException fe) {
+			fe.printStackTrace();
+		}
+		// System.out.println();
+	}
+
+}
